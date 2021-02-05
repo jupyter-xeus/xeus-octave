36184c554bb07cdb1c0e7e525341b1b79c74f1e2
Giulio Girardi <giulio.girardi@protechgroup.it>
2021-02-05 16:29:41 +0100

    Make plotstream helper functions inline
    Close: #23

--- src/toolkits/plotstream.hpp
+++ src/toolkits/plotstream.hpp
@@ -21,28 +21,25 @@
 #define PLOTSTREAM_H
 
 #include <octave/graphics.h>
+
 namespace xoctave {
 
-namespace {
-
-int getPlotStream(const graphics_object& o) {
+inline int getPlotStream(const graphics_object& o) {
 	return dynamic_cast<const figure::properties&>(o.get_ancestor("figure").get_properties())
 		.get___plot_stream__()
 		.int_value();
 }
 
-void setPlotStream(graphics_object& o, int p) {
+inline void setPlotStream(graphics_object& o, int p) {
 	if (o.isa("figure"))
 		dynamic_cast<figure::properties&>(o.get_properties()).set___plot_stream__(p);
 }
 
-void setPlotStream(const graphics_object& o, int p) {
+inline void setPlotStream(const graphics_object& o, int p) {
 	// deCONSTify the graphics_object
 	auto _go = o;
 	setPlotStream(_go, p);
 }
-
-}  // namespace
 
 }  // namespace xoctave
 
