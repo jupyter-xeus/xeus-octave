5cf0c5c189b797cfc0eccddefcef05ef2b4d5036
Giulio Girardi <giulio.girardi@protechgroup.it>
2023-01-27 18:33:07 +0000

    Headers

--- share/xeus-octave/+xwidgets/Widget.m
+++ share/xeus-octave/+xwidgets/Widget.m
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2023 Giulio Girardi.
+#
+# This file is part of xeus-octave.
+#
+# xeus-octave is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xeus-octave is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+#
+# along with xeus-octave.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 <%!
 from traitlets import (
     CaselessStrEnum,
